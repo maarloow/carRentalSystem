7f7b47aca2bdf1694336641705dc38bfe39ceba0
Robin Eriksson <eriksson_r@hotmail.com>
2026-07-23 10:13:08 +0300

    add initial car data

---
+++ script.js
@@ -0,0 +1,11 @@
+const cars = [
+    {
+        id: 1,
+        registration: "ABC123",
+        brand: "Volvo",
+        model: "V90",
+        year: 2022,
+        price: 89,
+        status: "Available"
+    }
+];
